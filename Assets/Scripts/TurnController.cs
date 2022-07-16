0bc31e40d09b9ac7adec4a6af5c3b0114e3acbbe
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-07-16 11:40:24 +0200

    TurnController merge fix

--- Assets/Scripts/TurnController.cs
+++ Assets/Scripts/TurnController.cs
@@ -2,16 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/TurnController.cs
 public class TurnController : MonoBehaviour
 {
-========
-public class CameraController : MonoBehaviour
-{
-    [SerializeField]
-    public GameObject player;
-
->>>>>>>> Player:Assets/Scripts/CameraController.cs
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +13,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<<< HEAD:Assets/Scripts/TurnController.cs
         
-========
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
->>>>>>>> Player:Assets/Scripts/CameraController.cs
     }
 }
