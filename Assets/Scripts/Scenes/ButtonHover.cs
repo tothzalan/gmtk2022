cccee5973aebb6ce34375650e92ace4b84e1ac2c
tothzalan <toth.zalan03@gmail.com>
2022-07-16 18:22:29 +0200

    StandardButton prefab hover
    Hovering highlights the button

---
+++ Assets/Scripts/Scenes/ButtonHover.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHover : MonoBehaviour
+{
+    Image shit;
+    Color oldColor;
+
+    void Start()
+    {
+        shit = gameObject.GetComponent<Image>();
+        oldColor  = shit.color;
+        /*
+        Color newColor = oldColor;
+        newColor.a = 0.8f;
+        shit.color = newColor;
+        */
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void MouseEnters()
+    {
+        Color newColor = oldColor;
+        newColor.a = 0.8f;
+        shit.color = newColor;
+    }
+
+    public void MouseLeaves()
+    {
+        shit.color = oldColor;
+    }
+}
