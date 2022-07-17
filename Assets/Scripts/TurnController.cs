175c0cac06a3588f7dc78e513f6d7bd81c4b4fb9
lackokaiser <43386382+lackokaiser@users.noreply.github.com>
2022-07-17 14:24:20 +0200

    Hud control

--- Assets/Scripts/TurnController.cs
+++ Assets/Scripts/TurnController.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Enemy;
 using UnityEngine;
 
@@ -9,12 +9,12 @@ public class TurnController : MonoBehaviour
     public List<EnemyMovement> EnemyMovements;
     public bool PlayerTurn = true;
     public bool FinalizedRoll = false;
-    public HudController Hud;
+    private HudController _hud;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _hud = gameObject.GetComponent<HudController>();
     }
     
     // Update is called once per frame
@@ -27,19 +27,24 @@ public class TurnController : MonoBehaviour
             
             BeginEnemyTurn();
         }
+
+        if (!PlayerTurn && EnemyMovements.Count == 0 || EnemyMovements.All(x => x.moveCount == 0))
+        {
+            // end of enemy turn
+        }
     }
 
     private void BeginPlayerTurn()
     {
         FinalizedRoll = false;
-        Hud.BeginPlayerTurn();
+        _hud.BeginPlayerTurn();
     }
 
     private void BeginEnemyTurn()
     {
-        Hud.BeginEnemyTurn();
+        _hud.BeginEnemyTurn();
 
-        int value = Hud.dice.RollDice();
+        int value = _hud.dice.RollDice();
         
         EnemyMovements.ForEach(x => x.moveCount = value);
     }
