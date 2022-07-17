c181eee5ab8912ab9fd399c6f9faabc1754abf64
lackokaiser <43386382+lackokaiser@users.noreply.github.com>
2022-07-17 11:22:45 +0200

    Dice/Hud controllers

---
+++ Assets/Scripts/Hud/HealthHudControl.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hud
+{
+    public class HealthHudControl : MonoBehaviour
+    {
+        public List<Sprite> healthBar;
+
+        private Image _image;
+    
+        private void Start()
+        {
+            _image = gameObject.GetComponent<Image>();
+        }
+
+        public void UpdateHealth(int health)
+        {
+            _image.sprite = healthBar[health];
+        }
+    }
+}
